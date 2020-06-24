0bca33460098af55aeb902c684c7398e2be31e24
Rogerio Caetano <cataua@gmail.com>
2020-06-23 21:39:24 -0300

    Correação server knefile

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -3,8 +3,6 @@ import Knex from 'knex';
 import { Model } from 'objection';
 const config = require('../knexfile');
 
-const config = require('../knexfile');
-
 const knex:Knex = Knex(config.development);
 
 Model.knex(knex);
