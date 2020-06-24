87e3e18af653842ea41df6d13a550d8cb36a2022
hedenica <hedenica@hotmail.com>
2020-06-24 08:26:23 -0300

    ✨  adding signup page #wip

---
+++ Front-End/src/pages/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const SignUp = () => {
+  return (
+    <Container>
+      <h1>SignUp Page</h1>
+    </Container>
+  )
+};
+
+export default SignUp;
