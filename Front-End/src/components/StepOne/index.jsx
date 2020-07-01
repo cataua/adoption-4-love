04385ab71810801af723d8c2061c490e76d07452
hedenica <hedenica@hotmail.com>
2020-06-30 21:06:35 -0300

    ♻️ refactored code

--- Front-End/src/components/StepOne/index.jsx
+++ Front-End/src/components/StepOne/index.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from 'react'
+import { Form } from '@unform/web';
 
-export default () => {
+import { Container, Content, AnimationContainer } from './styles';
+
+const StepOne = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <div>
-      <h3>Cadastre seu e-mail</h3>
-      <div className='row'>
-        <div className='six columns'>
-          <label>E-mail</label>
-          <input
-            className='u-full-width required'
-            placeholder='meu@email.com'
-            type='email'
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-            autoFocus
-          />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='six columns'>
-          <label>Senha</label>
-          <input
-            className='u-full-width'
-            placeholder='Digite sua senha'
-            type='password'
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <h3 style={{marginTop: 30}}>Cadastre e-mail e senha</h3>
+          <p>✉️</p>
+          <Form>
+            <div>
+              <div>
+                <input
+                  className=''
+                  placeholder='meu@email.com'
+                  type='email'
+                  onChange={e => setEmail(e.target.value)}
+                  value={email}
+                  autoFocus
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <input
+                  className=''
+                  placeholder='Digite sua senha'
+                  type='password'
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                />
+              </div>
+            </div>
+          </Form>
+        </AnimationContainer>
+      </Content>
+    </Container>
   )
 }
+
+export default StepOne;
